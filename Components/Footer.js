6d11aaacada1d2849dfc3843ec9c9e7d2abbd9b4
Allen <coryroberts5699@gmail.com>
2022-03-04 19:17:17 -0500

    Fixed some footer issues

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,12 +1,21 @@
+import { FaTwitterSquare } from "react-icons/fa"
+
 export default function Footer() {
   return (
     <>
       <center>
         <div className="footer">
+          <div className="subfooter">
             <h5>Email</h5>
-            <a></a>
+            <a href="mailto:email@email.com">Email@email.com</a>
+            </div>
+            <div className="subfooter">
+            <a href="mailto:email@email.com" className="icon"><FaTwitterSquare/></a>
+            </div> 
+            <div className="subfooter">
             <h5>Phone</h5>
-            <a></a>
+            <a>111-111-1111</a>
+            </div>
         </div>
       </center>
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link
             href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400&display=swap"
             rel="stylesheet"
